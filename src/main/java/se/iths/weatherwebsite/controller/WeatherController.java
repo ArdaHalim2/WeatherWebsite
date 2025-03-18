bcf68b4e8470d5a5c764c9d6e0302f0733df6dd9
ArdaHalim2 <arda.halim@iths.se>
2025-03-18 01:19:11 +0100

    Webserver connection

--- src/main/java/se/iths/weatherwebsite/controller/WeatherController.java
+++ src/main/java/se/iths/weatherwebsite/controller/WeatherController.java
@@ -1,4 +1,23 @@
 package se.iths.weatherwebsite.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import se.iths.weatherwebsite.service.WeatherService;
+
+@RestController
+@RequestMapping("/api/weather")
 public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping
+    public String getWeather(@RequestParam String city) {
+        return weatherService.fetchWeather(city);
+    }
 }
--- src/main/java/se/iths/weatherwebsite/service/WeatherService.java
+++ src/main/java/se/iths/weatherwebsite/service/WeatherService.java
@@ -1,4 +1,21 @@
 package se.iths.weatherwebsite.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class WeatherService {
+
+    @org.springframework.beans.factory.annotation.Value("${weatherapi.key}")
+    private String apiKey;
+
+    @Value("${weather.api.base-url}")
+    private String baseUrl;
+
+    public String fetchWeather(String city) {
+        String url = baseUrl + "?key=" + apiKey + "&q=" + city + "&aqi=no";
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, String.class);
+    }
 }
